43f298aaed1e989c6f45e5f15195ed96bd6f5c71
Gut <thiramate@nimblehq.co>
2020-09-18 15:23:38 +0700

    Update model to presence password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
