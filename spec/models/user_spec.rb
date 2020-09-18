e52df5281475f0606f487b1107f0336280636642
Gut <thiramate@nimblehq.co>
2020-09-18 15:24:17 +0700

    Add user model unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,38 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation test' do
+    it 'username presence' do
+      user = User.new(password: 'password', password_confirmation: 'password').save
+      expect(user).to eq(false)
+    end
+
+    it 'password presence' do
+      user = User.new(username: 'nimblehq', password_confirmation: 'password').save
+      expect(user).to eq(false)
+    end
+
+    it 'password confirmation presence' do
+      user = User.new(username: 'nimblehq', password: 'password').save
+      expect(user).to eq(false)
+    end
+
+    it 'password and password confirmation don\'t match' do
+      user = User.new(username: 'nimblehq', password: 'password', password_confirmation: 'password1').save
+      expect(user).to eq(false)
+    end
+
+    it 'username uniqueness' do
+      user1 = User.new(username: 'nimblehq', password: 'password', password_confirmation: 'password').save
+      expect(user1).to eq(true)
+
+      user2 = User.new(username: 'nimblehq', password: 'password', password_confirmation: 'password').save
+      expect(user2).to eq(false)
+    end
+
+    it 'password digest' do
+      user = User.new
+      assert_respond_to user, :password_digest
+    end
+  end
 end
