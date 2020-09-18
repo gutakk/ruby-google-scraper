174dca8465d155bc32809b03c3aabdd5d30718fc
Gut <thiramate@nimblehq.co>
2020-09-18 10:48:51 +0700

    Add users model, views and controller using rails generate scaffold

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/1
+  def show; end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit; end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    redirect_to users_url, notice: 'User was successfully destroyed.'
+  end
+end
