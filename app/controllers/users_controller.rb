8387e6a821797596d0172d0637c8c02c146834e1
Gut <thiramate@nimblehq.co>
2020-09-18 13:02:50 +0700

    Remove unused comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  # GET /users
   def index
     @users = User.all
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
 
@@ -24,7 +21,6 @@ class UsersController < ApplicationController
 
   private
 
-  # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
