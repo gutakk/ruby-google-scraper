f0b0e513d083ab3165e775f59d3c72562da56115
Gut <thiramate@nimblehq.co>
2020-09-21 20:18:33 +0700

    Update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, path: '/signup'
+  resources :users, only: :index
+  resource :users, only: :create, path: '/signup'
+  get '/signup', to: 'users#new'
 end
