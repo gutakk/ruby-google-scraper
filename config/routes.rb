10b9504d604bf040ecc6302f02639cdeaf99880a
Gut <thiramate@nimblehq.co>
2020-09-22 14:23:10 +0700

    Updater route to use root path to users index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: :index
+  resources :users, only: :index, path: '/'
   resource :users, only: :create, path: '/signup'
   get '/signup', to: 'users#new'
 end
