0a90d2ce0ba9138445efbde6a8d6d697d2453ccf
Gut <thiramate@nimblehq.co>
2020-09-22 16:53:56 +0700

    Add more test cases

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -90,6 +90,34 @@ describe 'Signup', type: :system do
       end
     end
 
+    it 'displays both duplicate user and password not match error' do
+      visit signup_path
+
+      within 'form' do
+        fill_in('user[username]', with: 'nimblehq')
+        fill_in('user[password]', with: 'password')
+        fill_in('user[password_confirmation]', with: 'password')
+
+        click_button('Signup')
+      end
+
+      visit signup_path
+
+      within 'form' do
+        fill_in('user[username]', with: 'nimblehq')
+        fill_in('user[password]', with: 'password')
+        fill_in('user[password_confirmation]', with: 'drowssap')
+
+        click_button('Signup')
+      end
+
+      expect(page).to have_selector('#errorExplanation')
+      within '#errorExplanation' do
+        expect(page).to have_content('Username has already been taken')
+        expect(page).to have_content('Password not match')
+      end
+    end
+
     it 'displays required message at username when submit empty string' do
       visit signup_path
 
