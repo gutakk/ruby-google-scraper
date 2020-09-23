5ae73a04f4cb50f04d27f986e70331d5fb8a7237
Gut <thiramate@nimblehq.co>
2020-09-23 18:20:13 +0700

    Update path to redirect when logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,11 @@
 class SessionsController < ApplicationController
   layout 'auth'
 
-  def new; end
-
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to users_path, notice: 'Logged in'
+      redirect_to root_path, notice: 'Logged in'
     else
       flash[:alert] = t('auth.logged_in_failed')
       render :new
