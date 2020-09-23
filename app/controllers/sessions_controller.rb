11d66b6a18dbd578cef3d30b9d1ffb498df02f6d
Gut <thiramate@nimblehq.co>
2020-09-23 11:15:14 +0700

    Update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
-  def new
-  end
+  layout 'auth'
+
+  def new; end
 
   def create
+    user = User.find_by(username: params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_path, notice: 'Logged in'
+    else
+      flash[:alert] = t('auth.logged_in_failed')
+      render :new
+    end
   end
 end
