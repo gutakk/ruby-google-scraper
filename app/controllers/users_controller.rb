4036fcca5555da36c19c105193a2576a216bf160
Gut <thiramate@nimblehq.co>
2020-09-23 11:42:22 +0700

    Update user controller to use locale

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_path, notice: 'Account was successfully created'
+      redirect_to users_path, notice: t('auth.signed_up_successfully')
     else
       render :new
     end
