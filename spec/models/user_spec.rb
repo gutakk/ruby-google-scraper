1494bae628ebd3c232d9ce29003ac802ed164223
Gut <thiramate@nimblehq.co>
2020-09-24 11:51:30 +0700

    Update user spec to use is_expected.to instead of should for syntax consistent with others

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,29 +5,29 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validation' do
     context 'presence' do
-      it { should validate_presence_of(:username) }
+      it { is_expected.to validate_presence_of(:username) }
 
-      it { should validate_presence_of(:password) }
+      it { is_expected.to validate_presence_of(:password) }
 
-      it { should validate_presence_of(:password_confirmation) }
+      it { is_expected.to validate_presence_of(:password_confirmation) }
     end
 
     context 'uniqueness' do
       subject { User.new(username: 'nimblehq', password: 'password', password_confirmation: 'password') }
 
-      it { should validate_uniqueness_of(:username) }
+      it { is_expected.to validate_uniqueness_of(:username) }
     end
 
     context 'confirmation' do
-      it { should validate_confirmation_of(:password).with_message(I18n.t('activerecord.errors.models.user.attributes.password_confirmation.confirmation')) }
+      it { is_expected.to validate_confirmation_of(:password).with_message(I18n.t('activerecord.errors.models.user.attributes.password_confirmation.confirmation')) }
     end
 
     context 'has secure password' do
-      it { should have_secure_password }
+      it { is_expected.to have_secure_password }
     end
 
     context 'length' do
-      it { should validate_length_of(:password) }
+      it { is_expected.to validate_length_of(:password) }
     end
   end
 end
