05249c82560b142a7dc772574c66633d750186ff
Gut <thiramate@nimblehq.co>
2020-09-24 11:28:41 +0700

    Add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def page_title
+    return [t('app.title'), (content_for :title_subfix)].join(' | ') if (content_for :title_subfix).present?
+
+    t('app.title')
+  end
 end
