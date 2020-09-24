0c9f3e2a7fde31c8ace605dee6c5ea7da58f46c1
Gut <thiramate@nimblehq.co>
2020-09-24 14:55:15 +0700

    Update signup to redirect to login page when signedup successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path, notice: t('auth.signed_up_successfully')
+      redirect_to login_path, notice: t('auth.signed_up_successfully')
     else
       render :new
     end
