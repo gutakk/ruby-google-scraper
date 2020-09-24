9c1673245b0ea4f8aedc824d809296604e95438f
Gut <thiramate@nimblehq.co>
2020-09-24 18:47:49 +0700

    Add session system spec

---
+++ spec/systems/sessions_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Sessions', type: :system do
+  describe 'login' do
+    context 'validates screen' do
+      it 'login screen should show correct field and button' do
+        visit login_path
+
+        within 'form' do
+          expect(page).to have_field('username')
+          expect(page).to have_field('password')
+          expect(page).to have_button(I18n.t('auth.login'))
+        end
+      end
+
+      it 'login screen should show correct label and components' do
+        visit login_path
+
+        expect(page).to have_selector('nav')
+        expect(page).to have_content(I18n.t('auth.login'))
+        expect(page).to have_content("#{I18n.t('app.do_not_have_account_yet')} #{I18n.t('auth.signup')}")
+
+        within 'form' do
+          expect(page).to have_content(I18n.t('auth.username'))
+          expect(page).to have_content(I18n.t('auth.password'))
+        end
+      end
+    end
+
+    context 'happy path' do
+      it 'given valid data should redirect to index page with greeting message' do
+        user = Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
+
+        visit login_path
+
+        within 'form' do
+          fill_in('username', with: user[:username])
+          fill_in('password', with: 'password')
+
+          click_button(I18n.t('auth.login'))
+        end
+
+        expect(current_path).to eql(root_path)
+        expect(page).to have_content("#{I18n.t('app.greeting')} #{user[:username]}")
+      end
+    end
+  end
+end
