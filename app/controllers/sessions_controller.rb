984915e376fa648054d5ad38dc6c9e4bb6d01dbe
Gut <thiramate@nimblehq.co>
2020-09-24 15:55:48 +0700

    Remove notice when redirect after logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: 'Logged in'
+      redirect_to root_path
     else
       flash.now[:alert] = t('auth.logged_in_failed')
       render :new
