f8fb0080f2b73112287a165eca800fa63fd42ff7
Gut <thiramate@nimblehq.co>
2020-09-25 12:30:16 +0700

    Add function to check if login then redirect to root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  private
+
   def current_user
     return unless session[:user_id]
 
     @current_user ||= User.find(session[:user_id])
+  end
+
+  def logged_in?
+    redirect_to root_path if current_user
   end
 end
