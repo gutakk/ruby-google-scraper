d65ec096742b075deeb1f8cb476b2aaac1c6375f
Gut <thiramate@nimblehq.co>
2020-09-25 10:39:05 +0700

    Update sessions system spec

--- spec/systems/sessions_spec.rb
+++ spec/systems/sessions_spec.rb
@@ -43,7 +43,10 @@ RSpec.describe 'Sessions', type: :system do
         end
 
         expect(current_path).to eql(root_path)
-        expect(page).to have_content("#{I18n.t('app.greeting')} #{user[:username]}")
+
+        within 'nav' do
+          expect(page).to have_content("#{I18n.t('app.greeting')} #{user[:username]}")
+        end
       end
 
       it 'clicks at signup link should redirect to correct page' do
