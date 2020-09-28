46f9055df563f446f101caf050eb59f1aca81821
Gut <thiramate@nimblehq.co>
2020-09-28 18:59:53 +0700

    Update upload csv to check both minimun and maximun range

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -10,14 +10,14 @@ class KeywordsController < ApplicationController
   def create
     file = params[:keyword][:file]
 
-    if CSV.read(file, headers: true).count > 1000
-      redirect_to keywords_path, alert: 'Your file contain more than 1000 keywords'
-    else
+    if CSV.read(file, headers: true).between?(1, 1000)
       CSV.foreach(file.path, headers: true) do |row|
         current_user.keywords.create(keyword: row[0])
       end
 
       redirect_to keywords_path, notice: 'Keyword uploaded successfully'
+    else
+      redirect_to keywords_path, alert: 'Your CSV file must contain between 1 to 1000 keywords'
     end
   end
 end
