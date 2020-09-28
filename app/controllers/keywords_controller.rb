ec78397ab55d4ca5f3497d316956b2f6d9e705c4
Gut <thiramate@nimblehq.co>
2020-09-28 18:42:57 +0700

    Update keyword controller to check if keyword more than 1000 then alert

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -8,10 +8,16 @@ class KeywordsController < ApplicationController
   end
 
   def create
-    CSV.foreach(params[:keyword][:file].path, headers: true) do |row|
-      current_user.keywords.create(keyword: row[0])
-    end
+    file = params[:keyword][:file]
 
-    redirect_to keywords_path, notice: 'Keyword uploaded successfully'
+    if CSV.read(file, headers: true).count > 1000
+      redirect_to keywords_path, alert: 'Your file contain more than 1000 keywords'
+    else
+      CSV.foreach(file.path, headers: true) do |row|
+        current_user.keywords.create(keyword: row[0])
+      end
+
+      redirect_to keywords_path, notice: 'Keyword uploaded successfully'
+    end
   end
 end
