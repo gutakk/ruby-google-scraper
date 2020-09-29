b83212d67cd798161c4f5f7ee029bcf4910bb2cb
Gut <thiramate@nimblehq.co>
2020-09-29 11:01:04 +0700

    Move keyword import logic to new method to fix ABC convention

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -4,20 +4,26 @@ require 'csv'
 
 class KeywordsController < ApplicationController
   def new
-    @keyword = Keyword.new
+    keyword = Keyword.new
   end
 
   def create
-    file = params[:keyword][:file]
+    @file = params[:keyword][:file]
 
-    if CSV.read(file, headers: true).count.between?(1, 1000)
-      CSV.foreach(file.path, headers: true) do |row|
-        current_user.keywords.create(keyword: row[0])
-      end
+    if CSV.read(@file, headers: true).count.between?(1, 1000)
+      import_keywords
 
-      redirect_to keywords_path, notice: 'Keyword uploaded successfully'
+      redirect_to keywords_path, notice: t('app.upload_csv_successfully')
     else
-      redirect_to keywords_path, alert: 'Your CSV file must contain between 1 to 1000 keywords'
+      redirect_to keywords_path, alert: t('app.invalid_csv')
+    end
+  end
+
+  private
+
+  def import_keywords
+    CSV.foreach(@file.path, headers: true) do |row|
+      current_user.keywords.create(keyword: row[0])
     end
   end
 end
