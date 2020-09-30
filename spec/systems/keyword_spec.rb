666910033e869c0b9d4ba519bd7774958687b6a8
Gut <thiramate@nimblehq.co>
2020-09-30 17:37:44 +0700

    Add upload csv spec to keyword system spec

--- spec/systems/keyword_spec.rb
+++ spec/systems/keyword_spec.rb
@@ -33,4 +33,28 @@ RSpec.describe Keyword, type: :system do
       end
     end
   end
+
+  describe 'uploads csv file' do
+    context 'given valid csv and user logged in' do
+      it 'displays uploaded successfully message' do
+        user = Fabricate(:user)
+        file_path = Rails.root.join('spec', 'fabricators', 'files', 'example.csv')
+
+        visit keywords_path
+
+        within 'form' do
+          fill_in('username', with: user[:username])
+          fill_in('password', with: 'password')
+
+          click_button(I18n.t('auth.login'))
+        end
+
+        attach_file('keyword[file]', file_path)
+
+        click_button(I18n.t('keyword.upload'))
+
+        expect(page).to have_content(I18n.t('keyword.upload_csv_successfully'))
+      end
+    end
+  end
 end
