c38e256bd135c36f6e33be74f75c51d924f95e61
Gut <thiramate@nimblehq.co>
2020-09-30 10:41:34 +0700

    Update sessions controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe 'GET#new' do
-    it 'renders a successful response' do
+    it 'returns a successful response' do
       get :new
 
       expect(response).to be_successful
@@ -16,7 +16,7 @@ RSpec.describe SessionsController, type: :controller do
       expect(response).to render_template(:new)
     end
 
-    it 'redirect to home when session exist' do
+    it 'redirects to home when session exists' do
       user = Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
       session[:user_id] = user[:id]
 
@@ -27,8 +27,8 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe 'POST#create' do
-    context 'with valid parameters' do
-      it 'username and password are valid' do
+    context 'given invalid parameters' do
+      it 'creates user id session and redirects to home page' do
         user = Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
 
         post :create, params: { username: user[:username], password: 'password' }
@@ -38,28 +38,57 @@ RSpec.describe SessionsController, type: :controller do
       end
     end
 
-    context 'with invalid parameters' do
-      before(:each) do
-        Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
+    context 'given invalid parameters' do
+      context 'given an invalid username' do
+        it 'does NOT set user_id to session' do
+          Fabricate(:user)
+
+          post :create, params: { username: 'qhelbmin', password: 'password' }
+
+          expect(session).to be_empty
+        end
+
+        it 'shows an alert flash' do
+          Fabricate(:user)
+
+          post :create, params: { username: 'qhelbmin', password: 'password' }
+
+          expect(flash[:alert]).to eql(I18n.t('auth.login_failed'))
+        end
+
+        it 'renders the template of :new action' do
+          Fabricate(:user)
+
+          post :create, params: { username: 'qhelbmin', password: 'password' }
+
+          expect(response).to render_template(:new)
+        end
       end
 
-      # Every test cases expect the same thing
-      after(:each) do
-        expect(session).to be_empty
-        expect(flash[:alert]).to eql(I18n.t('auth.login_failed'))
-        expect(response).to render_template(:new)
-      end
+      context 'given an invalid password' do
+        it 'does NOT set user_id to session' do
+          Fabricate(:user)
 
-      it 'username is invalid' do
-        post :create, params: { username: 'qhelbmin', password: 'password' }
-      end
+          post :create, params: { username: 'nimblehq', password: 'drowssap' }
 
-      it 'password in invalid' do
-        post :create, params: { username: 'nimblehq', password: 'dorwssap' }
-      end
+          expect(session).to be_empty
+        end
 
-      it 'both username and password are invalid' do
-        post :create, params: { username: 'qhelbmin', password: 'dorwssap' }
+        it 'shows an alert flash' do
+          Fabricate(:user)
+
+          post :create, params: { username: 'qhelbmin', password: 'drowssap' }
+
+          expect(flash[:alert]).to eql(I18n.t('auth.login_failed'))
+        end
+
+        it 'renders the template of :new action' do
+          Fabricate(:user)
+
+          post :create, params: { username: 'qhelbmin', password: 'drowssap' }
+
+          expect(response).to render_template(:new)
+        end
       end
     end
   end
