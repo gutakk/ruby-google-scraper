4fd712beb75475b04a2c8ea131f5e87f7ced514c
Gut <thiramate@nimblehq.co>
2020-09-30 12:09:58 +0700

    Update session routing spec

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -4,16 +4,10 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :routing do
   describe 'routing' do
-    it 'routes to #new' do
-      expect(get: '/login').to route_to('sessions#new')
-    end
+    it { expect(get: '/login').to route_to('sessions#new') }
 
-    it 'routes to #create' do
-      expect(post: '/sessions').to route_to('sessions#create')
-    end
+    it { expect(post: '/sessions').to route_to('sessions#create') }
 
-    it 'routes to #destroy' do
-      expect(delete: '/sessions').to route_to('sessions#destroy')
-    end
+    it { expect(delete: '/sessions').to route_to('sessions#destroy') }
   end
 end
