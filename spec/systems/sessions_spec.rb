c4f4a0bf1125f19b406e95c8fe7bc507c28df2e5
Gut <thiramate@nimblehq.co>
2020-09-30 12:27:56 +0700

    Update session system spec to not use before each hook

--- spec/systems/sessions_spec.rb
+++ spec/systems/sessions_spec.rb
@@ -2,83 +2,72 @@
 
 require 'rails_helper'
 
-describe 'login', type: :system do
-  context 'validates screen' do
-    it 'displays correct contents and components' do
-      visit login_path
+RSpec.describe 'Sessions', type: :system do
+  describe 'login' do
+    context 'validates screen' do
+      it 'displays correct contents and components' do
+        visit login_path
 
-      expect(page).to have_selector('nav')
-      expect(page).to have_content(I18n.t('auth.login'))
-      expect(page).to have_content("#{I18n.t('app.do_not_have_account_yet')} #{I18n.t('auth.signup')}")
+        expect(page).to have_selector('nav')
+        expect(page).to have_content(I18n.t('auth.login'))
+        expect(page).to have_content("#{I18n.t('app.do_not_have_account_yet')} #{I18n.t('auth.signup')}")
 
-      within 'form' do
-        expect(page).to have_content(I18n.t('auth.username'))
-        expect(page).to have_content(I18n.t('auth.password'))
+        within 'form' do
+          expect(page).to have_content(I18n.t('auth.username'))
+          expect(page).to have_content(I18n.t('auth.password'))
+        end
       end
     end
-  end
 
-  context 'given valid data to login form' do
-    it 'redirects to home page with greeting message' do
-      user = Fabricate(:user)
+    context 'given valid data to login form' do
+      it 'redirects to home page with greeting message' do
+        user = Fabricate(:user)
 
-      visit login_path
+        visit login_path
 
-      within 'form' do
-        fill_in('username', with: user[:username])
-        fill_in('password', with: 'password')
+        within 'form' do
+          fill_in('username', with: user[:username])
+          fill_in('password', with: 'password')
 
-        click_button(I18n.t('auth.login'))
-      end
+          click_button(I18n.t('auth.login'))
+        end
 
-      expect(current_path).to eql(root_path)
+        expect(current_path).to eql(root_path)
 
-      within 'nav' do
-        expect(page).to have_content("#{I18n.t('app.greeting')} #{user[:username]}")
+        within 'nav' do
+          expect(page).to have_content("#{I18n.t('app.greeting')} #{user[:username]}")
+        end
       end
     end
   end
 
   describe 'logout' do
-    before(:each) do
-      Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
+    context 'when logout clicked' do
+      it 'redirects to home page, displays logged out message and does NOT show greeting message and logout link in nav' do
+        Fabricate(:user)
 
-      visit root_path
+        visit root_path
 
-      within 'nav' do
-        click_link(I18n.t('auth.login'))
-      end
+        within 'nav' do
+          click_link(I18n.t('auth.login'))
+        end
 
-      within 'form' do
-        fill_in('username', with: 'nimblehq')
-        fill_in('password', with: 'password')
+        within 'form' do
+          fill_in('username', with: 'nimblehq')
+          fill_in('password', with: 'password')
 
-        click_button(I18n.t('auth.login'))
-      end
-    end
+          click_button(I18n.t('auth.login'))
+        end
 
-    it 'should have signout link in nav when session exist' do
-      within 'nav' do
-        expect(page).to have_selector('a', text: I18n.t('auth.logout'))
-      end
-    end
-
-    context 'logout clicked' do
-      before(:each) do
         within 'nav' do
           click_link(I18n.t('auth.logout'))
-        end
-      end
 
-      it 'clicks logout should show logged out message' do
-        expect(page).to have_content(I18n.t('auth.logout_successfully'))
-      end
-
-      it 'clicks logout should not show greeting message and logout link in nav' do
-        within 'nav' do
           expect(page).not_to have_content(I18n.t('app.greeting'))
           expect(page).not_to have_selector('a', text: I18n.t('auth.logout'))
         end
+
+        expect(current_path).to eql(root_path)
+        expect(page).to have_content(I18n.t('auth.logout_successfully'))
       end
     end
   end
