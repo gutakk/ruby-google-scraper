8d4720e3b411217fbd39e903413910c5f11cc582
Gut <thiramate@nimblehq.co>
2020-09-30 16:48:26 +0700

    Add redirects spec to keyword system spec

--- spec/systems/keyword_spec.rb
+++ spec/systems/keyword_spec.rb
@@ -2,5 +2,35 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Keywords', type: :system do
+RSpec.describe Keyword, type: :system do
+  describe 'redirects' do
+    context 'when user does not login yet' do
+      it 'redirects to login page with' do
+        visit keywords_path
+
+        expect(current_path).to eql(login_path)
+      end
+
+      it 'displays login required message' do
+        visit keywords_path
+
+        expect(page).to have_content(I18n.t('auth.login_required', page: I18n.t('keyword.page')))
+      end
+
+      it 'redirects to keywords path after logged in when come from keywords page' do
+        user = Fabricate(:user)
+
+        visit keywords_path
+
+        within 'form' do
+          fill_in('username', with: user[:username])
+          fill_in('password', with: 'password')
+
+          click_button(I18n.t('auth.login'))
+        end
+
+        expect(current_path).to eql(keywords_path)
+      end
+    end
+  end
 end
