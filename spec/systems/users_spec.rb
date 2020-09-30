32bd6555c50cc79e0b0c3970cb2d169755158c46
Gut <thiramate@nimblehq.co>
2020-09-30 11:56:32 +0700

    Update user system spec

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -2,59 +2,36 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :system do
-  describe 'signup' do
-    context 'validates screen' do
-      it 'displays signup screen should show correct field and button' do
-        visit signup_path
+describe 'signup', type: :system do
+  context 'validates screen' do
+    it 'displays correct contents and components' do
+      visit signup_path
 
-        within 'form' do
-          expect(page).to have_field('user[username]')
-          expect(page).to have_field('user[password]')
-          expect(page).to have_field('user[password_confirmation]')
-          expect(page).to have_button(I18n.t('auth.signup'))
-        end
-      end
+      expect(page).to have_selector('nav')
+      expect(page).to have_content(I18n.t('auth.signup'))
 
-      it 'displays signup screen show correct label and components' do
-        visit signup_path
-
-        expect(page).to have_selector('nav')
-        expect(page).to have_content(I18n.t('auth.signup'))
-
-        within 'form' do
-          expect(page).to have_content(I18n.t('auth.username'))
-          expect(page).to have_content(I18n.t('auth.password'))
-          expect(page).to have_content(I18n.t('auth.confirm_password'))
-        end
+      within 'form' do
+        expect(page).to have_content(I18n.t('auth.username'))
+        expect(page).to have_content(I18n.t('auth.password'))
+        expect(page).to have_content(I18n.t('auth.confirm_password'))
       end
     end
+  end
 
-    context 'happy path' do
-      it 'redirects to login page and show flash message' do
-        visit signup_path
+  context 'given valid data to signup form' do
+    it 'redirects to login page and show flash message' do
+      visit signup_path
 
-        within 'form' do
-          fill_in('user[username]', with: 'nimblehq')
-          fill_in('user[password]', with: 'password')
-          fill_in('user[password_confirmation]', with: 'password')
+      within 'form' do
+        fill_in('user[username]', with: 'nimblehq')
+        fill_in('user[password]', with: 'password')
+        fill_in('user[password_confirmation]', with: 'password')
 
-          click_button(I18n.t('auth.signup'))
-        end
-
-        expect(current_path).to eql(login_path)
-        expect(page).to have_content(I18n.t('auth.signup_successfully'))
+        click_button(I18n.t('auth.signup'))
       end
 
-      it 'clicks at login link should redirect to correct page' do
-        visit signup_path
-
-        within 'main' do
-          click_link(I18n.t('auth.login'))
-        end
-
-        expect(current_path).to eql(login_path)
-      end
+      expect(current_path).to eql(login_path)
+      expect(page).to have_content(I18n.t('auth.signup_successfully'))
     end
   end
 end
