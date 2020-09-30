acd9b670ce35a795c22c538609933e861cce8407
Gut <thiramate@nimblehq.co>
2020-09-30 15:39:32 +0700

    Update keywords controller spec to follow compass guideline

--- spec/controllers/keywords_controller_spec.rb
+++ spec/controllers/keywords_controller_spec.rb
@@ -4,72 +4,121 @@ require 'rails_helper'
 
 RSpec.describe KeywordsController, type: :controller do
   describe 'GET#new' do
-    # Logged in required before access to keywords controller
-    before(:each) do |test|
-      if test.metadata[:logged_in]
-        user = Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
+    context 'when user_id session exists' do
+      it 'returns a successful response' do
+        user = Fabricate(:user)
         session[:user_id] = user[:id]
+
+        get :new
+
+        expect(response).to be_successful
       end
 
-      get :new
+      it 'renders the template of :new action' do
+        user = Fabricate(:user)
+        session[:user_id] = user[:id]
+
+        get :new
+
+        expect(response).to render_template(:new)
+      end
     end
 
-    it 'renders a successful response', :logged_in do
-      expect(response).to be_successful
-    end
+    context 'when user_id session not exists' do
+      it 'redirects to login' do
+        get :new
 
-    it 'renders a correct template', :logged_in do
-      expect(response).to render_template(:new)
-    end
-
-    it 'redirect to login when not login' do
-      expect(response).to redirect_to(login_path)
+        expect(response).to redirect_to(login_path)
+      end
     end
   end
 
   describe 'POST#create' do
-    context 'with valid params' do
-      it 'valid csv file' do
-        user = Fabricate(:user, username: 'nimblehq', password: 'password', password_confirmation: 'password')
+    context 'given valid parameters (file)' do
+      it 'inserts keywords to database' do
+        user = Fabricate(:user)
         session[:user_id] = user[:id]
-
         file = fixture_file_upload('files/example.csv', 'text/csv')
 
         expect do
           post :create, params: { keyword: { file: file } }
         end.to change(Keyword, :count).by(6)
+      end
+
+      it 'redirects to keywords path' do
+        user = Fabricate(:user)
+        session[:user_id] = user[:id]
+        file = fixture_file_upload('files/example.csv', 'text/csv')
+
+        post :create, params: { keyword: { file: file } }
 
         expect(response).to redirect_to(keywords_path)
+      end
+
+      it 'shows a notice flash' do
+        user = Fabricate(:user)
+        session[:user_id] = user[:id]
+        file = fixture_file_upload('files/example.csv', 'text/csv')
+
+        post :create, params: { keyword: { file: file } }
+
         expect(flash[:notice]).to eql(I18n.t('keyword.upload_csv_successfully'))
       end
     end
 
-    context 'with invalid params' do
-      it 'not csv file' do
-        file = fixture_file_upload('files/nimble.png')
+    context 'given invalid parameters (file)' do
+      context 'given invalid file type' do
+        it 'redirects to keywords path' do
+          file = fixture_file_upload('files/nimble.png')
 
-        post :create, params: { keyword: { file: file } }
+          post :create, params: { keyword: { file: file } }
 
-        expect(response).to redirect_to(keywords_path)
-        expect(flash[:alert]).to eql(I18n.t('keyword.file_must_be_csv'))
+          expect(response).to redirect_to(keywords_path)
+        end
+
+        it 'shows an alert flash' do
+          file = fixture_file_upload('files/nimble.png')
+
+          post :create, params: { keyword: { file: file } }
+
+          expect(flash[:alert]).to eql(I18n.t('keyword.file_must_be_csv'))
+        end
       end
 
-      it 'no keywords csv file' do
-        file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+      context 'given no keyword csv' do
+        it 'redirects to keywords path' do
+          file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
 
-        post :create, params: { keyword: { file: file } }
+          post :create, params: { keyword: { file: file } }
 
-        expect(response).to redirect_to(keywords_path)
-        expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+          expect(response).to redirect_to(keywords_path)
+        end
+
+        it 'shows an alert flash' do
+          file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+
+          post :create, params: { keyword: { file: file } }
+
+          expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+        end
       end
 
-      it 'more than 1,000 keywords csv file' do
-        file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
+      context 'given more than 1,000 keywords csv' do
+        it 'redirects to keywords path' do
+          file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
 
-        post :create, params: { keyword: { file: file } }
+          post :create, params: { keyword: { file: file } }
 
-        expect(response).to redirect_to(keywords_path)
-        expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+          expect(response).to redirect_to(keywords_path)
+        end
+
+        it 'shows an alert flash' do
+          file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
+
+          post :create, params: { keyword: { file: file } }
+
+          expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+        end
       end
     end
   end
