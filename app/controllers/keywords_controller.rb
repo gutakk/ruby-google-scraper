ce122340425180ee68c7b80b5a27f41d0cc07339
Gut <thiramate@nimblehq.co>
2020-09-30 16:44:29 +0700

    Update login required message page to be generic

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -3,7 +3,7 @@
 require 'csv'
 
 class KeywordsController < ApplicationController
-  before_action :login_required, only: %i[new create]
+  before_action(only: %i[new create]) { login_required(t('keyword.page')) }
   before_action :fetch_file, only: :create
   before_action :csv?, only: :create
   before_action :csv_keyword_in_range?, only: :create
