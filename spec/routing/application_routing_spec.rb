5983349168394521da166666ba79b16769f7a718
Gut <thiramate@nimblehq.co>
2020-09-30 12:12:51 +0700

    Update routing spec

--- spec/routing/application_routing_spec.rb
+++ spec/routing/application_routing_spec.rb
@@ -4,8 +4,6 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController, type: :routing do
   describe 'routing' do
-    it 'routes to #home' do
-      expect(get: '/').to route_to('application#home')
-    end
+    it { expect(get: '/').to route_to('application#home') }
   end
 end
