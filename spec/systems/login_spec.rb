65aabd6799ac9394c7f734309ad30ea2dc80c2f2
Gut <thiramate@nimblehq.co>
2020-10-01 11:06:07 +0700

    Split login and logout system spec from session spec

---
+++ spec/systems/login_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Login', type: :system do
+  context 'validates screen' do
+    it 'displays correct contents and components' do
+      visit login_path
+
+      expect(page).to have_selector('nav')
+      expect(page).to have_content(I18n.t('auth.login'))
+      expect(page).to have_content("#{I18n.t('app.do_not_have_account_yet')} #{I18n.t('auth.signup')}")
+
+      within 'form' do
+        expect(page).to have_content(I18n.t('auth.username'))
+        expect(page).to have_content(I18n.t('auth.password'))
+      end
+    end
+  end
+
+  context 'given valid data to login form' do
+    it 'redirects to home page with greeting message' do
+      user = Fabricate(:user)
+
+      visit login_path
+
+      within 'form' do
+        fill_in('username', with: user[:username])
+        fill_in('password', with: 'password')
+
+        click_button(I18n.t('auth.login'))
+      end
+
+      expect(current_path).to eql(root_path)
+
+      within 'nav' do
+        expect(page).to have_content("#{I18n.t('app.greeting')} #{user[:username]}")
+      end
+    end
+  end
+end
