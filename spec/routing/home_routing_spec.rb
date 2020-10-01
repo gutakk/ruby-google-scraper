08a1d4b71e09574bece44b522245899a2c958990
Gut <thiramate@nimblehq.co>
2020-10-01 10:23:28 +0700

    Change application#home to home#index

--- spec/routing/home_routing_spec.rb
+++ spec/routing/home_routing_spec.rb
@@ -2,10 +2,10 @@
 
 require 'rails_helper'
 
-RSpec.describe ApplicationController, type: :routing do
+RSpec.describe HomeController, type: :routing do
   describe 'routing' do
     it 'routes to #home' do
-      expect(get: '/').to route_to('application#home')
+      expect(get: '/').to route_to('home#index')
     end
   end
 end
