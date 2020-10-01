961cbe1a78ec9ce797722b8167c4ec6ad067fd73
Gut <thiramate@nimblehq.co>
2020-10-01 14:30:43 +0700

    Update params name in user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    user = User.new(create_params)
 
     if user.save
       redirect_to login_path, notice: t('auth.signup_successfully')
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
+  def create_params
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
