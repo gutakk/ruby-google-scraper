fbbfb17f1e8501fd58156e5868dbc4b677b10a0b
Gut <thiramate@nimblehq.co>
2020-10-02 11:04:46 +0700

    Update keywords system spec to assert keywords table

--- spec/systems/keyword_spec.rb
+++ spec/systems/keyword_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe Keyword, type: :system do
   describe 'uploads csv file' do
     context 'given valid csv and user logged in' do
-      it 'displays uploaded successfully message' do
+      it 'displays uploaded successfully message, show keywords table and keywords must be ordered' do
         user = Fabricate(:user)
         file_path = Rails.root.join('spec', 'fabricators', 'files', 'example.csv')
 
@@ -23,6 +23,21 @@ RSpec.describe Keyword, type: :system do
         click_button(I18n.t('keyword.upload'))
 
         expect(page).to have_content(I18n.t('keyword.upload_csv_successfully'))
+        expect(page).to have_selector('table')
+
+        within 'table' do
+          within 'tbody' do
+            expect(page).to have_selector('tr', count: 6)
+
+            within 'tr:first-child' do
+              expect(page).to have_selector('td', text: 'Cristiano Ronaldo')
+            end
+
+            within 'tr:last-child' do
+              expect(page).to have_selector('td', text: 'Neymar')
+            end
+          end
+        end
       end
     end
   end
