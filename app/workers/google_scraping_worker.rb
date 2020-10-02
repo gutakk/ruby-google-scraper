e253470c74bcf87a39582444793ab8ec615625da
Gut <thiramate@nimblehq.co>
2020-10-02 15:34:47 +0700

    Add initialize to parse google page and add total links method

--- app/workers/google_scraping_worker.rb
+++ app/workers/google_scraping_worker.rb
@@ -1,10 +1,26 @@
 # frozen_string_literal: true
 
+require 'net/http'
+require 'Nokogiri'
+
 class GoogleScrapingWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
+  def initialize
+    uri = URI('https://www.google.com/search?q=AWS')
+    response = Net::HTTP.get(uri)
+    @parse_page ||= Nokogiri::HTML(response)
+  end
+
   def perform
-    p 'HELLO FROM GOOGLE SCRAPING WORKER'
+  end
+
+  private
+
+  def 
+
+  def total_links
+    @parse_page.css('a').map { |link| link['href'] }.length
   end
 end
