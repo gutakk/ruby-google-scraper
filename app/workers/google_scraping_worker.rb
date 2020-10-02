42dbb256207c98e4321e6e85991e131baccbb15f
Gut <thiramate@nimblehq.co>
2020-10-02 16:57:50 +0700

    Update google scraping worker to use HTTParty, set http user agent and define method to get information from scraping

--- app/workers/google_scraping_worker.rb
+++ app/workers/google_scraping_worker.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'net/http'
+require 'httparty'
 require 'Nokogiri'
 
 class GoogleScrapingWorker
@@ -9,18 +9,26 @@ class GoogleScrapingWorker
 
   def initialize
     uri = URI('https://www.google.com/search?q=AWS')
-    response = Net::HTTP.get(uri)
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+    'Chrome/85.0.4183.121 Safari/537.36'
+
+    response = HTTParty.get(uri, { headers: { 'User-Agent': user_agent } })
     @parse_page ||= Nokogiri::HTML(response)
   end
 
-  def perform
-  end
+  def perform; end
 
   private
 
-  def 
+  def total_top_position_adwords; end
 
-  def total_links
-    @parse_page.css('a').map { |link| link['href'] }.length
-  end
+  def top_position_adwords_links; end
+
+  def total_adwords; end
+
+  def total_non_adword_results; end
+
+  def non_adwords_links; end
+
+  def total_links; end
 end
