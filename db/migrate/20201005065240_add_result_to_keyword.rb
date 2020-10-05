5eb4610f7e077de0deb668ce3786c52108293985
Gut <thiramate@nimblehq.co>
2020-10-05 16:33:55 +0700

    Add adwords count column to keywords table

--- db/migrate/20201005065240_add_result_to_keyword.rb
+++ db/migrate/20201005065240_add_result_to_keyword.rb
@@ -6,6 +6,7 @@ class AddResultToKeyword < ActiveRecord::Migration[6.0]
     
     add_column :keywords, :status, :keyword_status, default: 'processing'
     add_column :keywords, :top_pos_adwords, :int
+    add_column :keywords, :adwords, :int
     add_column :keywords, :non_adwords, :int
     add_column :keywords, :links, :int
     add_column :keywords, :html_code, :text
