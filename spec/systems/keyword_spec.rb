540fa10336354d8a0db1d92f0dbe213f97338906
Gut <thiramate@nimblehq.co>
2020-10-07 17:00:45 +0700

    Remove unhappy path from keyword system spec

--- spec/systems/keyword_spec.rb
+++ spec/systems/keyword_spec.rb
@@ -3,37 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Keyword, type: :system do
-  describe 'redirects' do
-    context 'given an unauthenticated user' do
-      it 'redirects to login page with' do
-        visit keywords_path
-
-        expect(current_path).to eql(login_path)
-      end
-
-      it 'displays login required message' do
-        visit keywords_path
-
-        expect(page).to have_content(I18n.t('auth.login_required'))
-      end
-
-      it 'redirects to keywords path after logged in when come from keywords page' do
-        user = Fabricate(:user)
-
-        visit keywords_path
-
-        within 'form' do
-          fill_in('username', with: user[:username])
-          fill_in('password', with: 'password')
-
-          click_button(I18n.t('auth.login'))
-        end
-
-        expect(current_path).to eql(keywords_path)
-      end
-    end
-  end
-
   describe 'uploads csv file' do
     context 'given valid csv and user logged in' do
       it 'displays uploaded successfully message' do
