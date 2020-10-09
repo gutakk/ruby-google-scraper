7916f636379f87ea283f4a7eba6c0a8af49ad8a7
Gut <thiramate@nimblehq.co>
2020-10-09 10:29:47 +0700

    Add should show only user's keywords test case

--- spec/systems/view_keyword_list_spec.rb
+++ spec/systems/view_keyword_list_spec.rb
@@ -55,5 +55,45 @@ describe 'views keyword list', type: :system do
         end
       end
     end
+
+    it "displays ONLY logged in user's keywords" do
+      user1 = Fabricate(:user)
+      user2 = Fabricate(:user, username: 'hello', password: 'password', password_confirmation: 'password')
+      file_path = Rails.root.join('spec', 'fabricators', 'files', 'example.csv')
+
+      visit keywords_path
+
+      within 'form' do
+        fill_in('username', with: user1[:username])
+        fill_in('password', with: 'password')
+
+        click_button(I18n.t('auth.login'))
+      end
+
+      attach_file('keyword[file]', file_path)
+
+      click_button(I18n.t('keyword.upload'))
+
+      expect(current_path).to eql(keywords_path)
+      expect(page).to have_selector('table')
+
+      find('li.nav-item').click
+
+      within '.dropdown-menu' do
+        click_link(I18n.t('auth.logout'))
+      end
+
+      visit keywords_path
+
+      within 'form' do
+        fill_in('username', with: user2[:username])
+        fill_in('password', with: 'password')
+
+        click_button(I18n.t('auth.login'))
+      end
+
+      expect(current_path).to eql(keywords_path)
+      expect(page).not_to have_selector('table')
+    end
   end
 end
