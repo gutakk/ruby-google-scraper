4f9943d30b48d1ce2786f2890778f7c582960a36
Gut <thiramate@nimblehq.co>
2020-10-12 17:27:57 +0700

    Update keywords controller spec

--- spec/controllers/keywords_controller_spec.rb
+++ spec/controllers/keywords_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe KeywordsController, type: :controller do
   describe 'GET#index' do
-    context 'given the user_id session' do
+    context 'given authenticated user' do
       it 'returns a successful response' do
         user = Fabricate(:user)
         session[:user_id] = user[:id]
@@ -24,7 +24,7 @@ RSpec.describe KeywordsController, type: :controller do
       end
     end
 
-    context 'given NO user_id session' do
+    context 'given unauthenticated user' do
       it 'redirects to login' do
         get :index
 
@@ -34,102 +34,104 @@ RSpec.describe KeywordsController, type: :controller do
   end
 
   describe 'POST#create' do
-    context 'given valid parameters (file)' do
-      it 'inserts keywords to database' do
-        user = Fabricate(:user)
-        session[:user_id] = user[:id]
-        file = fixture_file_upload('files/example.csv', 'text/csv')
-
-        expect do
-          post :create, params: { csv_import_form: { file: file } }
-        end.to change(Keyword, :count).by(6)
-      end
-
-      it 'redirects to new keywords path' do
-        user = Fabricate(:user)
-        session[:user_id] = user[:id]
-        file = fixture_file_upload('files/example.csv', 'text/csv')
-
-        post :create, params: { csv_import_form: { file: file } }
-
-        expect(response).to redirect_to(keywords_path)
-      end
-
-      it 'shows a notice flash' do
-        user = Fabricate(:user)
-        session[:user_id] = user[:id]
-        file = fixture_file_upload('files/example.csv', 'text/csv')
-
-        post :create, params: { csv_import_form: { file: file } }
-
-        expect(flash[:notice]).to eql(I18n.t('keyword.upload_csv_successfully'))
-      end
-    end
-
-    context 'given invalid parameters (file)' do
-      context 'given invalid file type' do
-        it 'redirects to new keywords path' do
+    context 'given authenticated user' do
+      context 'given valid parameters (file)' do
+        it 'inserts keywords to database' do
           user = Fabricate(:user)
           session[:user_id] = user[:id]
-          file = fixture_file_upload('files/nimble.png')
+          file = fixture_file_upload('files/example.csv', 'text/csv')
+
+          expect do
+            post :create, params: { csv_import_form: { file: file } }
+          end.to change(Keyword, :count).by(6)
+        end
+
+        it 'redirects to keywords path' do
+          user = Fabricate(:user)
+          session[:user_id] = user[:id]
+          file = fixture_file_upload('files/example.csv', 'text/csv')
 
           post :create, params: { csv_import_form: { file: file } }
 
           expect(response).to redirect_to(keywords_path)
         end
 
-        it 'shows an alert flash' do
+        it 'shows a notice flash' do
           user = Fabricate(:user)
           session[:user_id] = user[:id]
-          file = fixture_file_upload('files/nimble.png')
+          file = fixture_file_upload('files/example.csv', 'text/csv')
 
           post :create, params: { csv_import_form: { file: file } }
 
-          expect(flash[:alert]).to eql(I18n.t('keyword.file_must_be_csv'))
+          expect(flash[:notice]).to eql(I18n.t('keyword.upload_csv_successfully'))
         end
       end
 
-      context 'given no keyword csv' do
-        it 'redirects to new keywords path' do
-          user = Fabricate(:user)
-          session[:user_id] = user[:id]
-          file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+      context 'given invalid parameters (file)' do
+        context 'given invalid file type' do
+          it 'redirects to keywords path' do
+            user = Fabricate(:user)
+            session[:user_id] = user[:id]
+            file = fixture_file_upload('files/nimble.png')
 
-          post :create, params: { csv_import_form: { file: file } }
+            post :create, params: { csv_import_form: { file: file } }
 
-          expect(response).to redirect_to(keywords_path)
+            expect(response).to redirect_to(keywords_path)
+          end
+
+          it 'shows an alert flash' do
+            user = Fabricate(:user)
+            session[:user_id] = user[:id]
+            file = fixture_file_upload('files/nimble.png')
+
+            post :create, params: { csv_import_form: { file: file } }
+
+            expect(flash[:alert]).to eql(I18n.t('keyword.file_must_be_csv'))
+          end
         end
 
-        it 'shows an alert flash' do
-          user = Fabricate(:user)
-          session[:user_id] = user[:id]
-          file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+        context 'given no keyword csv' do
+          it 'redirects to keywords path' do
+            user = Fabricate(:user)
+            session[:user_id] = user[:id]
+            file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
 
-          post :create, params: { csv_import_form: { file: file } }
+            post :create, params: { csv_import_form: { file: file } }
 
-          expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+            expect(response).to redirect_to(keywords_path)
+          end
+
+          it 'shows an alert flash' do
+            user = Fabricate(:user)
+            session[:user_id] = user[:id]
+            file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+
+            post :create, params: { csv_import_form: { file: file } }
+
+            expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+          end
         end
-      end
 
-      context 'given more than 1,000 keywords csv' do
-        it 'redirects to new keywords path' do
-          user = Fabricate(:user)
-          session[:user_id] = user[:id]
-          file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
+        context 'given more than 1,000 keywords csv' do
+          it 'redirects to keywords path' do
+            user = Fabricate(:user)
+            session[:user_id] = user[:id]
+            file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
 
-          post :create, params: { csv_import_form: { file: file } }
+            post :create, params: { csv_import_form: { file: file } }
 
-          expect(response).to redirect_to(keywords_path)
-        end
+            expect(response).to redirect_to(keywords_path)
+          end
 
-        it 'shows an alert flash' do
-          user = Fabricate(:user)
-          session[:user_id] = user[:id]
-          file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
+          it 'shows an alert flash' do
+            user = Fabricate(:user)
+            session[:user_id] = user[:id]
+            file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
 
-          post :create, params: { csv_import_form: { file: file } }
+            post :create, params: { csv_import_form: { file: file } }
 
-          expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+            expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+          end
         end
       end
     end
