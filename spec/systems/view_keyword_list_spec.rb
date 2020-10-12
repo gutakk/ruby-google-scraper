dd6ecd93a4bec58ab4dfdfd02bf8541c90fabadf
Gut <thiramate@nimblehq.co>
2020-10-12 15:54:53 +0700

    Separate keyword system spec into upload_csv_spec and view_keyword_list_spec

---
+++ spec/systems/view_keyword_list_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'views keyword list', type: :system do
+  context 'given authenticated user' do
+    it 'does NOT display table when no keywords' do
+      user = Fabricate(:user)
+
+      visit keywords_path
+
+      within 'form' do
+        fill_in('username', with: user[:username])
+        fill_in('password', with: 'password')
+
+        click_button(I18n.t('auth.login'))
+      end
+
+      expect(current_path).to eql(keywords_path)
+      expect(page).not_to have_selector('table')
+    end
+
+    it 'displays ordered keyword list table when upload successfully' do
+      user = Fabricate(:user)
+      file_path = Rails.root.join('spec', 'fabricators', 'files', 'example.csv')
+
+      visit keywords_path
+
+      within 'form' do
+        fill_in('username', with: user[:username])
+        fill_in('password', with: 'password')
+
+        click_button(I18n.t('auth.login'))
+      end
+
+      attach_file('keyword[file]', file_path)
+
+      click_button(I18n.t('keyword.upload'))
+
+      expect(current_path).to eql(keywords_path)
+      expect(page).to have_selector('table')
+
+      within 'table' do
+        within 'tbody' do
+          expect(page).to have_selector('tr', count: 6)
+
+          tr_list = all('tr')
+
+          expect(tr_list[0]).to have_selector('td', text: 'Cristiano Ronaldo')
+          expect(tr_list[1]).to have_selector('td', text: 'Eden Hazard')
+          expect(tr_list[2]).to have_selector('td', text: 'Kevin De Bruyne')
+          expect(tr_list[3]).to have_selector('td', text: 'Kylian Mbappe')
+          expect(tr_list[4]).to have_selector('td', text: 'Lionel Messi')
+          expect(tr_list[5]).to have_selector('td', text: 'Neymar')
+        end
+      end
+    end
+  end
+end
