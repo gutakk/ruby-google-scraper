bd776c698c91fb331cb17afc000327c153a0b0c8
Gut <thiramate@nimblehq.co>
2020-10-12 17:58:37 +0700

    Update keyword route spec

--- spec/routing/keywords_routing_spec.rb
+++ spec/routing/keywords_routing_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe KeywordsController, type: :routing do
   describe 'routing' do
     it { expect(get: '/keywords').to route_to('keywords#index') }
 
+    it { expect(get: '/keywords/1234').to route_to('keywords#show', id: '1234') }
+
     it { expect(post: '/keywords').to route_to('keywords#create') }
   end
 end
