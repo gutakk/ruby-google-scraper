50a5fc1988b694c59064c437d8ba29a82a2a8161
Gut <thiramate@nimblehq.co>
2020-10-20 10:41:10 +0700

    Remove test

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -4,14 +4,6 @@ require 'rails_helper'
 
 RSpec.describe Keyword, type: :model do
   describe 'association' do
-    context 'has many' do
-      it { is_expected.to have_many(:top_position_adword_links) }
-    end
-
-    context 'has many' do
-      it { is_expected.to have_many(:non_adword_links) }
-    end
-
     context 'belong to' do
       it { is_expected.to belong_to(:user) }
     end
