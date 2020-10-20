538ebf2211418685ea974f56ebe1482658cf3717
Gut <thiramate@nimblehq.co>
2020-10-20 10:40:00 +0700

    Remove association

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -3,8 +3,5 @@
 class Keyword < ApplicationRecord
   belongs_to :user
 
-  has_many :top_position_adword_links, dependent: :destroy
-  has_many :non_adword_links, dependent: :destroy
-
   enum status: { in_queue: 0, completed: 1, failed: 2 }
 end
