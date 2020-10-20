9d8ed7956e8a24bd5d7604c31310c0779ebef483
Gut <thiramate@nimblehq.co>
2020-10-20 10:39:19 +0700

    Remove top position adword links and non adword links model

--- app/models/non_adword_link.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class NonAdwordLink < ApplicationRecord
-  belongs_to :keyword
-end
--- app/models/top_position_adword_link.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class TopPositionAdwordLink < ApplicationRecord
-  belongs_to :keyword
-end
