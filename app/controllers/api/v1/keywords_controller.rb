3bcd824e8db897df9530bde83424091af7dc6318
Gut <thiramate@nimblehq.co>
2020-10-28 17:13:34 +0700

    Add create action for keywords controller API

--- app/controllers/api/v1/keywords_controller.rb
+++ app/controllers/api/v1/keywords_controller.rb
@@ -4,20 +4,44 @@ module Api
   module V1
     class KeywordsController < ApplicationController
       before_action :doorkeeper_authorize!
-      before_action :current_user
+      before_action :load_user
+      before_action :set_csv_import_form, only: :create
 
       def index
         render json: search_keyword, status: :ok
       end
 
       def show
-        render json: @user.keywords.find(params[:id])
+        render json: @user.keywords.find(params[:id]), status: :ok
+      end
+
+      def create
+        if @csv_import_form.save(create_params)
+          ScrapingProcessDistributingJob.perform_later(@csv_import_form.inserted_keywords.rows)
+
+          render json: {
+            message: I18n.t('keyword.upload_csv_successfully')
+          }, status: :ok
+        else
+          render json: {
+            message: 'Upload csv file unsuccessfully',
+            errors: @csv_import_form.errors.messages[:base][0]
+          }, status: :bad_request
+        end
       end
 
       private
 
-      def current_user
+      def load_user
         @user = User.find(doorkeeper_token.resource_owner_id)
+      end
+
+      def set_csv_import_form
+        @csv_import_form = CsvImportForm.new(user: @user)
+      end
+
+      def create_params
+        params.permit(:file)
       end
 
       def search_keyword
