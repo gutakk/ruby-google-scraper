7600f0e1d101519d2705cdd04012c08532d5e2c9
Gut <thiramate@nimblehq.co>
2020-10-28 18:07:45 +0700

    Add custom unauthorize response

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -5,6 +5,10 @@ module Api
     class ApplicationController < ActionController::API
       include Localization
       include Api::V1::ResponseHandler
+
+      def doorkeeper_unauthorized_render_options(error: nil)
+        { json: error }
+      end
     end
   end
 end
