062c44369fd63d26716f5c82a5eb6ee84d6bda5e
Gut <thiramate@nimblehq.co>
2020-10-29 11:28:12 +0700

    Add index action test spec for keywords controller API

---
+++ spec/controllers/api/v1/keywords_controller_spec.rb
@@ -0,0 +1,356 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::KeywordsController, type: :controller do
+  describe 'GET#index' do
+    context 'given access token' do
+      it 'returns ok status code' do
+        user = Fabricate(:user)
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, resource_owner_id: user.id, application_id: application.id)
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'returns keywords for valid resource owner' do
+        user = Fabricate(:user)
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, resource_owner_id: user.id, application_id: application.id)
+        Fabricate(:keyword, user_id: user.id, keyword: 'Lionel Messi')
+        Fabricate(:keyword, user_id: user.id, keyword: 'Cristiano Ronaldo')
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(JSON.parse(response.body).count).to eql 2
+      end
+
+      it 'does NOT return keywords for invalid resource owner' do
+        user1 = Fabricate(:user)
+        user2 = Fabricate(:user, username: 'hello', password: 'password', password_confirmation: 'password')
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, resource_owner_id: user2.id, application_id: application.id)
+        Fabricate(:keyword, user_id: user1.id, keyword: 'Lionel Messi')
+        Fabricate(:keyword, user_id: user1.id, keyword: 'Cristiano Ronaldo')
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(JSON.parse(response.body).count).to eql 0
+      end
+    end
+
+    context 'given NO access token' do
+      it 'returns unauthorized status code' do
+        get :index
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns invalid token message' do
+        get :index
+
+        expect(JSON.parse(response.body)['error']['name']).to eql('invalid_token')
+        expect(JSON.parse(response.body)['description']).to eql(I18n.t('doorkeeper.errors.messages.invalid_token.unknown'))
+      end
+    end
+
+    context 'given wrong access token' do
+      it 'returns unauthorized status code' do
+        request.headers['Authorization'] = 'xxx'
+
+        get :index
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns invalid token message' do
+        request.headers['Authorization'] = 'xxx'
+
+        get :index
+
+        expect(JSON.parse(response.body)['error']['name']).to eql('invalid_token')
+        expect(JSON.parse(response.body)['description']).to eql(I18n.t('doorkeeper.errors.messages.invalid_token.unknown'))
+      end
+    end
+
+    context 'given revoked access token' do
+      it 'returns unauthorized status code' do
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, application_id: application.id, revoked_at: DateTime.now)
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns token was revoked message' do
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, application_id: application.id, revoked_at: DateTime.now)
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(JSON.parse(response.body)['error']['name']).to eql('invalid_token')
+        expect(JSON.parse(response.body)['description']).to eql(I18n.t('doorkeeper.errors.messages.invalid_token.revoked'))
+      end
+    end
+
+    context 'given expired access token' do
+      it 'returns unauthorized status code' do
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, application_id: application.id, expires_in: 0)
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(response).to have_http_status(:unauthorized)
+      end
+
+      it 'returns token was expired message' do
+        application = Fabricate(:application)
+        access_token = Fabricate(:access_token, application_id: application.id, expires_in: 0)
+
+        request.headers['Authorization'] = "Bearer #{access_token.token}"
+
+        get :index
+
+        expect(JSON.parse(response.body)['error']['name']).to eql('invalid_token')
+        expect(JSON.parse(response.body)['description']).to eql(I18n.t('doorkeeper.errors.messages.invalid_token.expired'))
+      end
+    end
+  end
+
+  # describe 'GET#show' do
+  #   context 'given the authenticated user' do
+  #     context 'given correct keyword id' do
+  #       it 'returns a successful response' do
+  #         user = Fabricate(:user)
+  #         keyword = Fabricate(:keyword, user_id: user.id, keyword: 'test')
+  #         session[:user_id] = user.id
+
+  #         get :show, params: { id: keyword.id }
+
+  #         expect(response).to be_successful
+  #       end
+
+  #       it 'renders the template of :show action' do
+  #         user = Fabricate(:user)
+  #         keyword = Fabricate(:keyword, user_id: user.id, keyword: 'test')
+  #         session[:user_id] = user.id
+
+  #         get :show, params: { id: keyword.id }
+
+  #         expect(response).to render_template(:show)
+  #       end
+  #     end
+
+  #     context 'given incorrect keyword id' do
+  #       it 'raises ActiveRecord::RecordNotFound' do
+  #         user = Fabricate(:user)
+  #         session[:user_id] = user.id
+
+  #         expect do
+  #           get :show, params: { id: 'not_found_id' }
+  #         end.to raise_error(ActiveRecord::RecordNotFound)
+  #       end
+  #     end
+  #   end
+
+  #   context 'given unauthenticated user' do
+  #     it 'redirects to login' do
+  #       user = Fabricate(:user)
+  #       keyword = Fabricate(:keyword, user_id: user.id, keyword: 'test')
+
+  #       get :show, params: { id: keyword.id }
+
+  #       expect(response).to redirect_to(login_path)
+  #     end
+  #   end
+  # end
+
+  # describe 'POST#create' do
+  #   context 'given authenticated user' do
+  #     context 'given valid parameters (file)' do
+  #       it 'inserts keywords to database' do
+  #         user = Fabricate(:user)
+  #         session[:user_id] = user.id
+  #         file = fixture_file_upload('files/example.csv', 'text/csv')
+
+  #         expect do
+  #           post :create, params: { csv_import_form: { file: file } }
+  #         end.to change(Keyword, :count).by(6)
+  #       end
+
+  #       it 'redirects to keywords path' do
+  #         user = Fabricate(:user)
+  #         session[:user_id] = user.id
+  #         file = fixture_file_upload('files/example.csv', 'text/csv')
+
+  #         post :create, params: { csv_import_form: { file: file } }
+
+  #         expect(response).to redirect_to(keywords_path)
+  #       end
+
+  #       it 'shows a notice flash' do
+  #         user = Fabricate(:user)
+  #         session[:user_id] = user.id
+  #         file = fixture_file_upload('files/example.csv', 'text/csv')
+
+  #         post :create, params: { csv_import_form: { file: file } }
+
+  #         expect(flash[:notice]).to eql(I18n.t('keyword.upload_csv_successfully'))
+  #       end
+
+  #       it 'creates google scraping job' do
+  #         user = Fabricate(:user)
+  #         session[:user_id] = user.id
+  #         file = fixture_file_upload('files/example.csv', 'text/csv')
+
+  #         expect do
+  #           post :create, params: { csv_import_form: { file: file } }
+  #         end.to have_enqueued_job(ScrapingProcessDistributingJob)
+  #       end
+  #     end
+
+  #     context 'given invalid parameters (file)' do
+  #       context 'given invalid file type' do
+  #         it 'redirects to keywords path' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/nimble.png')
+
+  #           post :create, params: { csv_import_form: { file: file } }
+
+  #           expect(response).to redirect_to(keywords_path)
+  #         end
+
+  #         it 'shows an alert flash' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/nimble.png')
+
+  #           post :create, params: { csv_import_form: { file: file } }
+
+  #           expect(flash[:alert]).to eql(I18n.t('keyword.file_must_be_csv'))
+  #         end
+
+  #         it 'does NOT insert keywords to database' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/nimble.png')
+
+  #           expect do
+  #             post :create, params: { csv_import_form: { file: file } }
+  #           end.to change(Keyword, :count).by(0)
+  #         end
+
+  #         it 'does NOT create google scraping worker sidekiq job' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/nimble.png')
+
+  #           expect do
+  #             post :create, params: { csv_import_form: { file: file } }
+  #           end.not_to have_enqueued_job(ScrapingProcessDistributingJob)
+  #         end
+  #       end
+
+  #       context 'given no keyword csv' do
+  #         it 'redirects to keywords path' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+
+  #           post :create, params: { csv_import_form: { file: file } }
+
+  #           expect(response).to redirect_to(keywords_path)
+  #         end
+
+  #         it 'shows an alert flash' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/no_keywords.csv', 'text/csv')
+
+  #           post :create, params: { csv_import_form: { file: file } }
+
+  #           expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+  #         end
+
+  #         it 'does NOT insert keywords to database' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/no_keywords.csv')
+
+  #           expect do
+  #             post :create, params: { csv_import_form: { file: file } }
+  #           end.to change(Keyword, :count).by(0)
+  #         end
+
+  #         it 'does NOT create google scraping worker sidekiq job' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/no_keywords.csv')
+
+  #           expect do
+  #             post :create, params: { csv_import_form: { file: file } }
+  #           end.not_to have_enqueued_job(ScrapingProcessDistributingJob)
+  #         end
+  #       end
+
+  #       context 'given more than 1,000 keywords csv' do
+  #         it 'redirects to keywords path' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
+
+  #           post :create, params: { csv_import_form: { file: file } }
+
+  #           expect(response).to redirect_to(keywords_path)
+  #         end
+
+  #         it 'shows an alert flash' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/more_than_thoudsand_keywords.csv', 'text/csv')
+
+  #           post :create, params: { csv_import_form: { file: file } }
+
+  #           expect(flash[:alert]).to eql(I18n.t('keyword.keyword_range'))
+  #         end
+
+  #         it 'does NOT insert keywords to database' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/more_than_thoudsand_keywords.csv')
+
+  #           expect do
+  #             post :create, params: { csv_import_form: { file: file } }
+  #           end.to change(Keyword, :count).by(0)
+  #         end
+
+  #         it 'does NOT create google scraping worker sidekiq job' do
+  #           user = Fabricate(:user)
+  #           session[:user_id] = user.id
+  #           file = fixture_file_upload('files/more_than_thoudsand_keywords.csv')
+
+  #           expect do
+  #             post :create, params: { csv_import_form: { file: file } }
+  #           end.not_to have_enqueued_job(ScrapingProcessDistributingJob)
+  #         end
+  #       end
+  #     end
+  #   end
+  # end
+end
---
+++ spec/fabricators/application_fabricator.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Fabricator(:application, from: 'Doorkeeper::Application') do
+  name { FFaker.name }
+  redirect_uri 'https://nniimmbblleehhqq.com'
+end
