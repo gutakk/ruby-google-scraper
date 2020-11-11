167afee0906c2f43b850949e26acdb1b729eba5c
Gut <thiramate@nimblehq.co>
2020-11-11 12:33:38 +0700

    Update routing test case spec

--- spec/routing/api/v1/users_routing_spec.rb
+++ spec/routing/api/v1/users_routing_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :routing do
   describe 'routing' do
-    it { expect(post: '/api/v1/users').to route_to('api/v1/users#create') }
+    it 'routes /api/v1/users to api v1 users controller' do
+      expect(post: '/api/v1/users').to route_to('api/v1/users#create')
+    end
   end
 end
