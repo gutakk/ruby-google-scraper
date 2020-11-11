abaff5e476ab21e09783a9588c794a5788380010
Gut <thiramate@nimblehq.co>
2020-11-11 12:16:38 +0700

    Add routes for keywords api

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :users, only: :create
+      resources :keywords, only: %i[index show create]
     end
   end
 end
