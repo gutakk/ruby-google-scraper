3d00b9a8447fe0adf53789c856d0c662d3bad51e
Gut <thiramate@nimblehq.co>
2020-11-11 12:39:53 +0700

    Update keywords routing spec name

--- spec/routing/api/v1/keywords_routing_spec.rb
+++ spec/routing/api/v1/keywords_routing_spec.rb
@@ -4,10 +4,16 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::KeywordsController, type: :routing do
   describe 'routing' do
-    it { expect(get: 'api/v1/keywords').to route_to('api/v1/keywords#index') }
+    it 'gets /api/v1/keywords to api v1 keywords controller, index action' do
+      expect(get: 'api/v1/keywords').to route_to('api/v1/keywords#index')
+    end
 
-    it { expect(get: 'api/v1/keywords/1234').to route_to('api/v1/keywords#show', id: '1234') }
+    it 'gets /api/v1/keywords/<id> to api v1 keywords controller, show action' do
+      expect(get: 'api/v1/keywords/1234').to route_to('api/v1/keywords#show', id: '1234')
+    end
 
-    it { expect(post: 'api/v1/keywords').to route_to('api/v1/keywords#create') }
+    it 'post /api/v1/keywords to api v1 keywords controller, create action' do
+      expect(post: 'api/v1/keywords').to route_to('api/v1/keywords#create')
+    end
   end
 end
--- spec/routing/api/v1/users_routing_spec.rb
+++ spec/routing/api/v1/users_routing_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :routing do
   describe 'routing' do
-    it 'routes /api/v1/users to api v1 users controller' do
+    it 'post /api/v1/users to api v1 users controller, create action' do
       expect(post: '/api/v1/users').to route_to('api/v1/users#create')
     end
   end
